ea4c91b34eca2750dccf63a18ea3d5d457f60835
Lenz-devops <florent.lauber@gmail.com>
2020-02-28 10:56:06 +0100

    some change

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
+  access_key = "AKIAXED33KBXYYCG2KNX"
+  secret_key = "Z3OL3njwigFzv8uIIm0ASzL1X7sJ8hPOfGn0i21M"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-062f7200baf2fa504"
   instance_type = "t2.micro"
 }
 
