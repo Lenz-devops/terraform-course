1b99ba96624fc69534fbe2f2bee6362ae961926c
Lenz-devops <florent.lauber@gmail.com>
2020-04-05 18:36:35 +0200

    ../demo-4/vars.tf

--- demo-5/vars.tf
+++ demo-5/vars.tf
@@ -5,9 +5,9 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
+    us-east-1 = "ami-09a5b0b7edf08843d"
     us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    eu-west-1 = "ami-034d940df32c75d15"
   }
 }
 
